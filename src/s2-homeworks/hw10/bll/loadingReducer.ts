34bef5c0dd41b5a23190d8e89467805bac998964
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2023-05-10 15:19:27 +0300

    hw11 10.05.23

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,25 +1,19 @@
 const initState = {
     isLoading: false,
 }
-
 export type InitStateType = typeof initState
-
 export const loadingReducer = (state = initState, action: LoadingActionType): InitStateType => { // fix any
     switch (action.type) {
-
         case "CHANGE_LOADING":
             return {...state, isLoading: action.isLoading}
-
         default:
             return state
     }
 }
-
 type LoadingActionType = {
     type: 'CHANGE_LOADING'
     isLoading: boolean
 }
-
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
     isLoading,
